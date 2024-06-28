54a56965d12e62f28a485b9c2594a7fa3a61d4db
momika <fmomika@gmail.com>
2024-06-28 12:09:33 +0200

    ajout des corps gras dans le tableau

--- src/app/model/huile.ts
+++ src/app/model/huile.ts
@@ -2,8 +2,26 @@ export default
 class Huile{
     nom:String|null
     id:Number|null
-    constructor(unNom:String,uneId:Number){
+    iode:Number|null
+    ins:Number|null
+    douceur:Number|null
+    lavant:Number|null
+    durete:Number|null
+    solubilite:Number|null
+    sechage:Number|null
+
+
+    constructor(unNom:String,uneId:Number,unIode:Number,
+                unIns:Number,uneDouceur:Number,unLavant:Number,
+                uneDurete:Number,uneSolubilite:Number,unSechage:Number){
         this.id=uneId
         this.nom=unNom
+        this.iode=unIode
+        this.ins=unIns
+        this.douceur=uneDouceur
+        this.lavant=unLavant
+        this.durete=uneDurete
+        this.solubilite=uneSolubilite
+        this.sechage= unSechage
     }
 }
--- src/app/services/huiles-services.service.ts
+++ src/app/services/huiles-services.service.ts
@@ -8,6 +8,9 @@ export class HuilesServicesService {
   /**
    * l'attribut huiles de ce service contient tout les corps gras qui viendrait de notre api / base de données
    */
-  huiles:Array<Huile>= [{id: 1, nom: "huile Coco"},{id: 2,nom: "huile d'olives"}]
+  huiles:Array<Huile>= [{id: 1, nom: "huile Coco",iode: 9,ins: 248,douceur: 7.746,
+                        lavant: 14.462,durete: 9.390,solubilite: 11.204,sechage: 11.880},
+                        {id: 2,nom: "huile d'olives",iode: 78,ins: 111,douceur: 9.260,
+                        lavant: 10.192,durete: 10.144,solubilite: 9.298,sechage: 10.194}]
   constructor() { }
 }
